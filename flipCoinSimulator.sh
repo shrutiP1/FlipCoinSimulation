ebb8b5a34f91a69fa6b5307e376af946af5d793f
shrutiP1 <shrutipatil13798@gmail.com>
2021-04-21 23:37:37 +0530

    Show number of time head or tail won

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -14,3 +14,19 @@ else
      echo "Tails"
 fi
 
+
+#UC2
+headCount=0;
+tailCount=0;
+for (( i=0 ; i<10 ; i++ ))
+do
+   res=$((RANDOM%2))
+  if [ $res -eq 1 ]
+  then
+        ((headCount++))
+  else
+      ((tailCount++))
+  fi
+done
+    echo "number of time head won " $headCount
+    echo "number of time tail won"  $tailCount
