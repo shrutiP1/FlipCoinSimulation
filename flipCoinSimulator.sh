1570b0e26fd3b9ba2103a7e3f08c0ec67d946c1f
shrutiP1 <shrutipatil13798@gmail.com>
2021-04-22 00:11:19 +0530

    Show if its win or tie and if win then by how much

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -30,3 +30,29 @@ do
 done
     echo "number of time head won " $headCount
     echo "number of time tail won"  $tailCount
+
+#UC3
+headCount=0;
+tailCount=0;
+
+while [[ $headCount -le 21 &&  $tailCount -le 21 ]]
+do
+    res=$((RANDOM%2))
+  if [ $res -eq 1 ]
+  then
+        ((headCount++))
+  else
+      ((tailCount++))
+  fi
+done
+echo "headcount is " $headCount
+echo "tailcount is " $tailCount
+if [ $headCount -eq $tailCount ]
+then
+     echo "its tie"
+elif [  $headCount -gt $tailCount ]
+then
+     echo "Head won by " $(($headCount-$tailCount))
+else
+     echo "tail won by " $(($tailCount-$headCount))
+fi
